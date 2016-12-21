9d7089c5dbc82905295601334d913a40d39ffa64
popoko0102 <pissenlit.izm@gmail.com>
2016-12-21 06:36:55 +0000

    try tasks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
   def show # 追加
    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+    #> User.new
+    #<User id: nil, name: nil, email: nil, password_digest: nil, created_at: nil, updated_at: nil> 
   end
 
   def create
@@ -16,12 +20,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    #> User.find(1)
+    #User Load (0.5ms)  SELECT  "users".* FROM "users" WHERE "users"."id" = ? LIMIT 1  [["id", 1]]
+    #<User id: 1, name: "あいうえお", email: "aiueokakikukeko@gmails.com", password_digest: "$2a$10$CzS7jLUYD1ZHWsMpLtkc.eE2YROaodJfg5m.O2fXSZZ...", created_at: "2016-12-16 00:47:55", updated_at: "2016-12-16 00:47:55"> 
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Update Profile"
+      redirect_to @user # ここを修正 。redirect_to user_path(@user)　と同じ
+    else
+      render 'edit'
+    end
+  end
 
   private
-
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                :password_confirmation)
   end
 
 end
